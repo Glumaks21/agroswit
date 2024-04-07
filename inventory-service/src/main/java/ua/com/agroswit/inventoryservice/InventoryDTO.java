cdb54965f991986085c3369448cd58804209f02f
Masya <glumaks21@gmail.com>
2024-04-07 21:28:28 +0300

    Added configuration server

--- inventory-service/src/main/java/ua/com/agroswit/inventoryservice/InventoryDTO.java
+++
@@ -1,6 +0,0 @@
-package ua.com.agroswit.inventoryservice;
-
-public record InventoryDTO(
-        Integer product1CId,
-        Integer quantity) {
-}
--- inventory-service/src/main/resources/Init DDL.sql
+++ inventory-service/src/main/resources/Init DDL.sql
@@ -6,3 +6,6 @@ create table inventory
     constraint inventory_prod_1с_id foreign key
         (product_1с_id) references product (article_1c_id)
 );
+
+alter table inventory
+    add constraint inventory_quant_not_negative check ( quantity >= 0 );
